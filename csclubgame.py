141fc0052512e6cb3b7bed7c70fd230e10d6cf07
mools04 <56056790+mools04@users.noreply.github.com>
2019-10-02 09:35:57 -0400

    Update csclubgame.py
    fixed bugs

--- csclubgame.py
+++ csclubgame.py
@@ -6,14 +6,23 @@ print ("Okay, transporting you to", x)
 
 time.sleep(2)
 
-if x == "The Ocean":{
-    
-    print("You are on a ship in the middle of the pacific. What do you do?")
-}
-
-y = input("")
 if x == "The Castle":{
 
     print("You are at the foot of a drawbridge of a large English castle.")
 }
 
+if x == "The Ocean":{
+    
+    print("You are on a ship in the middle of the pacific. What do you do?")
+}
+
+else:{
+    
+    print("Please restart game")   
+}
+
+y = input("You may go North, South, East, or West")
+if y == ("North" or "South" or "East" or "West"):{
+    
+    print("You are now travelling", y)
+}
