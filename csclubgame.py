d26d3e8d79f83ed9d73bb33137350901a5cbb59d
mools04 <56056790+mools04@users.noreply.github.com>
2019-10-02 08:42:31 -0400

    Update csclubgame.py

--- csclubgame.py
+++ csclubgame.py
@@ -1,33 +1,19 @@
 import time
-print ("Welcome to *Game Title*!")
+print("Welcome to *Game Title*!")
 
 x = input("Where would you like to go?")
+print ("Okay, transporting you to", x)
+
+time.sleep(2)
 
 if x == "The Ocean":{
-
-    print ("You are on a ship in the middle of the pacific. What do you do?")
-
+    
+    print("You are on a ship in the middle of the pacific. What do you do?")
 }
+
+y = input("")
 if x == "The Castle":{
 
-    print ("You are at the foot of a drawbridge of a large english castle. What do you do?")
-    //sleep//
-    z = input("You may travel North, South, East, or West.")
-    if z == "North":{
-    //insert code here//
-    }
-    if z == "South":{
-        
-    }
-    if z == "East":{
-        
-    }
-    if z == "West":{
-        
-    }
+    print("You are at the foot of a drawbridge of a large English castle.")
 }
-else:{
 
-    print ("Try Again.")
-
-}
