beb79583773c5dccaf62e3c5553f7b50347578a5
mools04 <56056790+mools04@users.noreply.github.com>
2019-10-01 22:56:23 -0400

    Started the game
    It has some syntax errors

---
+++ csclubgame.py
@@ -0,0 +1,33 @@
+import time
+print ("Welcome to *Game Title*!")
+
+x = input("Where would you like to go?")
+
+if x == "The Ocean":{
+
+    print ("You are on a ship in the middle of the pacific. What do you do?")
+
+}
+if x == "The Castle":{
+
+    print ("You are at the foot of a drawbridge of a large english castle. What do you do?")
+    //sleep//
+    z = input("You may travel North, South, East, or West.")
+    if z == "North":{
+    //insert code here//
+    }
+    if z == "South":{
+        
+    }
+    if z == "East":{
+        
+    }
+    if z == "West":{
+        
+    }
+}
+else:{
+
+    print ("Try Again.")
+
+}
