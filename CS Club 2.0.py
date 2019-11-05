81c8dbf95a8c86c6250120503a190d590384a224
Daniellu24 <56607543+Daniellu24@users.noreply.github.com>
2019-11-05 15:23:34 -0500

    Update CS Club 2.0.py

--- CS Club 2.0.py
+++ CS Club 2.0.py
@@ -7,27 +7,27 @@ info = pygame.display.Info()
 SIZE = (info.current_w, info.current_h)
 LENGTH = SIZE[0]
 
-RED = [250, 65, 65]
 TURQUOISE = [65, 234, 250]
 GOLD = [209, 207, 98]
 GREEN = [98, 209, 98]
-SILVER = [196, 202, 206]
 BLACK = [0, 0, 0]
 
 x = 250
 y = 250
 r = 100
+BEFORECLICK = [196, 202, 206]
+AFTERCLICK = [250, 65, 65]
 
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("CS Club Game")
 
 def draw():
     screen.fill(GOLD)
-    pygame.draw.circle(screen, SILVER, (x, y), r)
+    pygame.draw.circle(screen, BEFORECLICK, (x, y), r)
 
 def drawtext():
     font = pygame.font.SysFont('Consolas.ttf', 85)
-    text = font.render("Welcome to *Game Title!*", True, BLACK)
+    text = font.render("Welcome to the circle game!", True, BLACK)
     screen.blit(text, [50, 50])
     
 run = True
@@ -35,7 +35,11 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+            
+    click = pygame.mouse.get_pressed()
+    if click = [1]:
+        pygame.draw.circle(screen, AFTERCLICK, (x, y), r)
+        
     curs = pygame.mouse.get_pos()
     (x, y) = curs
     
