ed8512168560bed264f569f5e962e90dc0d80916
mools04 <56056790+mools04@users.noreply.github.com>
2019-11-05 15:51:00 -0500

    Update CS Club 2.0.py

--- CS Club 2.0.py
+++ CS Club 2.0.py
@@ -1,4 +1,3 @@
-#This works, but it's boring and we need to add to it
 import pygame
 import os
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" %(0, 20)
@@ -15,6 +14,7 @@ BLACK = [0, 0, 0]
 x = 250
 y = 250
 r = 100
+
 BEFORECLICK = [196, 202, 206]
 AFTERCLICK = [250, 65, 65]
 
@@ -26,25 +26,25 @@ def draw():
     pygame.draw.circle(screen, BEFORECLICK, (x, y), r)
 
 def drawtext():
-    font = pygame.font.SysFont('Consolas.ttf', 85)
+    font = pygame.font.Font('Consolas.ttf', 85)
     text = font.render("Welcome to the circle game!", True, BLACK)
-    screen.blit(text, [50, 50])
+    screen.blit(text, [400, 540])
+    font1 = pygame.font.Font('Consolas.ttf', 30)
+    text1 = font1.render("By Myles, David and Daniel", True, BLACK)
+    screen.blit(text1, [400, 650])
     
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            
-    click = pygame.mouse.get_pressed()
-    if click = [1]:
-        pygame.draw.circle(screen, AFTERCLICK, (x, y), r)
-        
+    draw()
     curs = pygame.mouse.get_pos()
     (x, y) = curs
-    
-    draw()
     drawtext()
+    if pygame.mouse.get_pressed()[0]:
+        pygame.draw.circle(screen, AFTERCLICK, (x, y), r)
+        
     pygame.display.flip()
     #We should add something here to make it more interesting
 
